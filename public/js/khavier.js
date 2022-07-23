138d412e7ed86909ed0fa674c1761a4d3a5686c6
Kharamel <KharamelMustacheMachine@gmail.com>
2022-07-23 12:38:44 +0200

    Perfect sized

--- public/js/khavier.js
+++ public/js/khavier.js
@@ -1,19 +1,32 @@
-const music = new Audio('../sound/music/Pierre.mp3');
-const bpm = 100;
-var dx;
-var speed = 10;
-var score = 0;
-var ticker;
-var board_height;
-var tile_height;
-var lastTick;
+let dx;
+let score = 0;
+let ticker;
+let board_height;
+let tile_height;
+let lastTick;
 
-var moveTilesDownTicker = 0;
-var generateTileTicker = 0;
-var speed = 60000/bpm;
-var tileSpeed;
+let moveTilesDownTicker = 0;
+let generateTileTicker = 0;
+let speed;
+let tileSpeed;
+let row_offset = 0;
 
-var startTick;
+
+const MAX_ROWS = 8;
+let rowid = 0;
+
+function getTileHeight(){
+    var tile = $("<div class='tile' id='temp'/>").on("click", tilePressed)
+    $(".line").append(tile);
+    let height = tile.height();
+    tile.remove();
+    return height;
+}
+
+function setBPM(bpm) {
+    speed = 60000 / bpm; // ms between each tile creation
+    tileSpeed = speed * dx / tile_height; // ms between each 1 percent tile move
+}
 
 function prepareBoard() {
     score = 0
@@ -22,84 +35,120 @@ function prepareBoard() {
     $(".popup-container").addClass("hidden");
 
     board_height = $(".board").removeClass("hidden").height();
-    tile_height = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--tile-height")) / 100;
-    tile_height *= board_height;
-    speed = 60000/bpm; // ms between each tile creation
-    dx = board_height/100; // 1 percent of the board each time
-    tileSpeed = speed/((tile_height/dx)); // ms between each 1 percent tile move
-
-    console.log((tile_height/dx));
-
+    tile_height = board_height*2/MAX_ROWS;
+    dx = board_height / 100; // 1 percent of the board each time
+    row_offset = 0;
+    setBPM(100);
 
     lastTick = Date.now();
-    ticker = setInterval(tick,1)
-    music.play();
-    music.playbackRate=1.25
+    ticker = setInterval(tick, 10)
 }
 
-
-function tick(){
+function tick() {
     var now = Date.now();
     var deltaTime = now - lastTick; // in ms
     lastTick = now;
-    moveTilesDownTicker+=deltaTime;
-    generateTileTicker+=deltaTime;
+    moveTilesDownTicker += deltaTime;
+    generateTileTicker += deltaTime;
 
-    if(generateTileTicker >= speed){
+    if (moveTilesDownTicker >= tileSpeed) {
+        moveTilesDown(Math.floor(moveTilesDownTicker / tileSpeed));
+        moveTilesDownTicker %= tileSpeed;
+    }
+    if (generateTileTicker >= speed) {
+        generateTileTicker -= speed;
         generateTile();
-        generateTileTicker-=speed;
-        //moveTilesDown(25);
-    }
-    if(moveTilesDownTicker >= tileSpeed){
-        moveTilesDown(moveTilesDownTicker / tileSpeed);
-        moveTilesDownTicker%=tileSpeed;
     }
 }
 
 
-function generateTile(){
-    createTile((Math.floor(Math.random() * $(".line").length) + 1));
-    //createTile((Math.floor(Math.random() * 2) + 1));
+function generateTile() {
+    let n = $(".line").length
+    let counter = 0;
+    let tabIndex = [];
+    for (let i = 1; i <= n; i++) {
+        if (Math.random() < (i - counter) / n) {
+            tabIndex.push(i);
+            counter++;
+            if (Math.random() < 0.8) break;
+        }
+    }
+    createTiles(tabIndex);
 }
 
-function createTile(index){
-    var tile = $("<div class='tile'/>").on("click", tilePressed);
-    $(".line:nth-child("+ index +")").append(tile);
+
+
+
+function createTiles(tabIndex) {
+
+    for (let i = 0; i < tabIndex.length; i++) {
+        let index = tabIndex[i];
+
+        let note = 'c3'
+        switch (index) {
+            case 1:
+                note = 'e3';
+                break;
+            case 2:
+                note = 'g3';
+                break;
+            case 3:
+                note = 'c4';
+                break;
+        }
+        var tile = $("<div class='tile' data-note='" + note + "' data-rowid='"+rowid+"'/>").on("click", tilePressed)
+            .css("height", tile_height + 'px');
+        $(".line:nth-child(" + index + ")").append(tile);
+    }
+    rowid++;
+    rowid%=MAX_ROWS;
 }
 
-function tilePressed(){
+function tilePressed(e) {
     score++;
     $("#score").text(score);
+    new Audio('../sound/notes/' + $(e.target).data('note') + '.mp3').play()
+    /*new Audio('../sound/notes/c3.mp3').play()
+    new Audio('../sound/notes/e3.mp3').play()
+    new Audio('../sound/notes/g3.mp3').play()
+    new Audio('../sound/notes/c4.mp3').play()
+    */
+
     /*if(startTick === undefined){
         startTick = Date.now();
     }
     $("#score").text("BPM : " + ((score-1)/((Date.now()-startTick)/60000)));*/
     $(this).remove();
+    //clearInterval(ticker);
+
 }
 
-function moveTilesDown(amount){
 
+function moveTilesDown(amount) {
+    let board_length = 2*board_height;
+    row_offset += (dx * amount);
+    row_offset%=board_length;
     $(".tile").css('top', function (index, curValue) {
-        curValue = (parseFloat(curValue) + (dx*amount));
-        if(curValue > board_height){
+        let lastValue = parseFloat(curValue);
+        let rowid = parseInt($(this).data('rowid'));
+        curValue = (((row_offset - rowid*tile_height)+board_length) % board_length) - board_height;
+        if (curValue < lastValue) {
             gameOver();
-        }else{
+        } else {
             return curValue + 'px';
         }
+
     });
 }
 
-function gameOver(){
+function gameOver() {
     clearInterval(ticker);
     $(".tile").remove();
     $("#score").addClass("hidden");
     $(".popup-container").removeClass("hidden");
     $(".board").addClass("hidden");
-    music.pause();
-    music.currentTime = 0;
 }
 
-$("#start").on("click", ()=>{
-    console.log("AA");
+$("#start").on("click", () => {
     prepareBoard();
 });
